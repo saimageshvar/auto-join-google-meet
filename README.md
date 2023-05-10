5135b68ebe7c5b614529b88e074b720cdb2a072c
saimageshvar <saimageshvar@chronus.com>
2023-05-10 15:54:08 +0530

    Working condition

---
+++ README.md
@@ -0,0 +1,4 @@
+This extension will do the following
+  * Turn off the video automatically before joining
+  * Joins the meeting only if someone else joins the meeting
+  * Waits if the room is empty
---
+++ content.js
@@ -0,0 +1,30 @@
+function start() {
+	url = location.href;
+	if (url.includes("meet.google.com")) {
+		its_meet();
+	}
+}
+
+function its_meet() {
+	const interval = setInterval(function() {
+		try {
+			const joinNowElement = [...document.querySelectorAll("span")].find((ele) => ele.textContent === "Join now");
+			const divs = [...document.querySelectorAll('div')];
+			const noParticipants = divs.find(e => e.textContent === "No one else is here");
+			const someoneIsPresent = divs.find(e => e.textContent.match(/is in this call/));
+			if(joinNowElement){
+        const turnOffVideo = document.querySelector('div[aria-label="Turn off camera (ctrl + e)"]');
+				if(turnOffVideo) {
+					turnOffVideo.click();
+				}
+				if(!noParticipants && someoneIsPresent){
+					joinNowElement.click();
+					clearInterval(interval);
+				}
+      }
+		} catch (err) {
+			console.log("Error", err);
+		}
+	}, 2000)
+}
+start();
